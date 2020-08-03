fe904c18359536f40f2ae2db373fea97ff1988b1
SlavaDobr <v.dobrinin@RMS.LOCAL>
2020-08-03 19:52:06 +0300

    changed assignment of user data

--- SignatureFactory.java
+++ SignatureFactory.java
@@ -7,6 +7,7 @@ import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.PdfStamper;
+import org.apache.commons.lang3.StringUtils;
 import ru.ruselprom.signs.data.PdfData;
 import ru.ruselprom.signs.data.UserData;
 import ru.ruselprom.signs.exceptions.SignaturesAppRuntimeException;
@@ -14,16 +15,23 @@ import ru.ruselprom.signs.exceptions.SignaturesAppRuntimeException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class SignatureFactory {
     private static final String BASE_FONT = "C:\\ptc\\Windchill_11.1cps05\\eclipse\\fonts\\GOST type A.ttf";
     private static final String SINGS_PATH = "C:\\ptc\\Windchill_11.1cps05\\Windchill\\codebase\\netmarkets\\images\\sings\\";
+    private List<RoleData> roleDataList;
     private List<String> roles;
     private List<String> users;
     private List<String> dates;
 
     public SignatureFactory(UserData userData) {
+        roleDataList = Arrays.asList(new RoleData(PromotionNoticeProcess.DEV_ROLE,0),
+                                     new RoleData("Проверить",14),
+                                     new RoleData("Согласовать",27),
+                                     new RoleData("Нормоконтроль",56),
+                                     new RoleData("Утвердить",70));
         this.roles = userData.getRoles();
         this.users = userData.getUsers();
         this.dates = userData.getDates();
@@ -34,7 +42,7 @@ public class SignatureFactory {
         if (roles == null || users == null || dates == null) {
             throw new SignaturesAppRuntimeException("userData is null");
         }
-        if (roles.size() != users.size() || roles.size() != users.size()) {
+        if (roles.size() != users.size() || dates.size() != users.size()) {
             throw new SignaturesAppRuntimeException("userData is fail");
         }
     }
@@ -54,16 +62,28 @@ public class SignatureFactory {
 
             float x;
             float y;
-            int userPosition;
+            StringBuilder addedRoles = new StringBuilder();
             for (int i = 0; i < users.size(); i++) {
-                userPosition = getPositionRole(roles, i);
+                RoleData roleData = getRoleData(roles.get(i));
+                if (roleData == null) {
+                    continue;
+                }
+                if (addedRoles.toString().contains(roleData.roleName)) {
+                    if (roleData.roleName.equals("Согласовать") &&
+                            StringUtils.countMatches(addedRoles, "Согласовать") == 1) {
+                        roleData.deltaY = roleData.deltaY + 14;
+                    } else {
+                        continue;
+                    }
+                }
+                addedRoles.append(roleData.roleName);
                 stream.setFontAndSize(bf, 12.0F);
                 x = pageSize.getRight() - 486;
-                y = pageSize.getBottom() + 87 - getDeltaY(i);
+                y = pageSize.getBottom() + 87 - roleData.deltaY;
                 stream.setTextMatrix(x, y);
-                stream.showText(users.get(userPosition));
+                stream.showText(users.get(i));
 
-                Image waterMarkImage = Image.getInstance(SINGS_PATH + users.get(userPosition) + ".png");
+                Image waterMarkImage = Image.getInstance(SINGS_PATH + users.get(i) + ".png");
                 x = pageSize.getRight() - 415;
                 waterMarkImage.scaleAbsolute(25, 25);
                 waterMarkImage.setAbsolutePosition(x, (y - 10));
@@ -72,7 +92,7 @@ public class SignatureFactory {
                 stream.setFontAndSize(bf, 8.0F);
                 x = pageSize.getRight() - 380;
                 stream.setTextMatrix(x, y);
-                stream.showText(dates.get(userPosition));
+                stream.showText(dates.get(i));
             }
             stream.endText();
             stamper.setFullCompression();
@@ -89,20 +109,22 @@ public class SignatureFactory {
         }
     }
 
-    private int getDeltaY(int i) {
-        final int[] y = new int[]{0, 14, 27, 56, 70};
-        return y[i];
+    private RoleData getRoleData(String role) {
+        for (RoleData roleData : roleDataList) {
+            if (role.contains(roleData.roleName)) {
+                return roleData;
+            }
+        }
+        return null;
     }
 
-    private int getPositionRole(List<String> roles, int i) {
-        final String[] allRoles = new String[]{
-                PromotionNoticeProcess.DEV_ROLE,
-                "Проверить",
-                "Согласовать",
-                "Нормоконтроль",
-                "Утвердить"
-        };
+    private class RoleData {
+        private String roleName;
+        private int deltaY;
 
-        return roles.indexOf(allRoles[i]);
+        public RoleData(String roleName, int deltaY) {
+            this.roleName = roleName;
+            this.deltaY = deltaY;
+        }
     }
 }
